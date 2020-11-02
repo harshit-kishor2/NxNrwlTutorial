59db9e124e1779c476b4f891e11cd87227492e6c
Harshit Kishore <harshitkishore@singsys.com>
2020-11-02 14:06:55 +0530

    fix-error

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -50,9 +50,9 @@ app.listen(port, () => {
 
 //==============================================================================================
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("dist/apps/frontend"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist","apps","frontend", "index.html"));
   });
 }
